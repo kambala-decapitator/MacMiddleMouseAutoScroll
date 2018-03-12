2e3b96a84893d900e5eb837f80b5d3deeb788b05
Andrey Filipenkov <decapitator@ukr.net>
2018-03-12 19:02:26 +0300

    find AXScrollArea in the clicked location & dump its properties to console

--- MacMiddleMouseAutoScroll/AppDelegate.m
+++ MacMiddleMouseAutoScroll/AppDelegate.m
@@ -10,11 +10,64 @@
 
 @interface AppDelegate ()
 @property (weak) IBOutlet NSWindow *window;
+@property (nonatomic, weak) id monitor;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    NSLog(@"accessibility enabled: %d", AXIsProcessTrustedWithOptions((CFDictionaryRef)@{(__bridge NSString *)kAXTrustedCheckOptionPrompt: @YES})); // 10.9+
+
+    self.monitor = [NSEvent addGlobalMonitorForEventsMatchingMask:NSEventMaskOtherMouseDown handler:^(NSEvent * _Nonnull event) {
+        if (event.buttonNumber != 2) { // handle only middle button click
+            return;
+        }
+
+        AXUIElementRef sysElement = AXUIElementCreateSystemWide(), curElement;
+        AXUIElementCopyElementAtPosition(sysElement, NSEvent.mouseLocation.x, NSEvent.mouseLocation.y, &curElement);
+        CFRelease(sysElement);
+
+        while (curElement) {
+            CFTypeRef role;
+            AXUIElementCopyAttributeValue(curElement, kAXRoleAttribute, &role);
+            BOOL isScrollArea = CFStringCompare(role, kAXScrollAreaRole, kNilOptions) == kCFCompareEqualTo;
+            CFRelease(role);
+            if (isScrollArea)
+                break;
+
+            AXUIElementRef parentElement;
+            AXUIElementCopyAttributeValue(curElement, kAXParentAttribute, (CFTypeRef *)&parentElement);
+            CFRelease(curElement);
+            curElement = parentElement;
+        }
+        if (!curElement)
+            return;
+
+        AXUIElementRef scrollAreaElement = curElement;
+        NSMutableString *attributesStr;
+        CFArrayRef attributes;
+        if (AXUIElementCopyAttributeNames(scrollAreaElement, &attributes) == kAXErrorSuccess) {
+            attributesStr = [NSMutableString new];
+            for (NSString *attribute in (__bridge NSArray *)attributes) {
+                CFTypeRef value;
+                BOOL hasValue = AXUIElementCopyAttributeValue(scrollAreaElement, (CFStringRef)attribute, &value) == kAXErrorSuccess;
+                [attributesStr appendFormat:@"\n%@ = %@", attribute, value];
+                if (hasValue)
+                    CFRelease(value);
+            }
+            CFRelease(attributes);
+        }
+        NSLog(@"scrollarea %@ attributes:%@", scrollAreaElement, attributesStr);
+        CFRelease(scrollAreaElement);
+    }];
+}
+
+- (void)applicationWillTerminate:(NSNotification *)notification {
+    [NSEvent removeMonitor:self.monitor];
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+    return YES;
 }
 
 @end
